643640a934fb0a65cc08745bf3a6e152b095c150
jarret <jarret@vm.(none)>
2012-12-05 16:37:53 -0600

    Rails & Bootstrap
    Basic initializers.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ghint::Application.config.secret_token = 'dccd0f802c35fbb825df33ffef868b5213c4f678b24ede756a7ac15dae5ecf18ec6f1b700ac77431bf1fb42973f0f062054c23b12a09fe89efc8716a9213769d'
