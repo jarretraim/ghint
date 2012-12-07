a22424baddcf366a8906616b24f09177f0f9c115
Jarret Raim <jarret.raim@rackspace.com>
2012-12-07 09:22:06 -0600

    Fixed Tests
    Added devise helpers for Test::Unit and wrote valid fixture for users
    class. Doesn't test much now, but build succeeds.

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
   test "should get index" do
+    sign_in :user, users(:valid)
+
     get :index
     assert_response :success
   end
