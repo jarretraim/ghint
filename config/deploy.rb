d874437d2efc74f1c6eb588b9d7a7bf4c8b101b3
Jarret Raim <jarret.raim@rackspace.com>
2012-12-14 09:30:33 -0600

    Deployment Fixes
    Ignore any public/assets from testing precompile steps, added configs
    needed for rvm and capistrano. Added flag to turn off devise trying to
    talk to the database during precompile.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,9 @@
+set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
+
+require 'rvm/capistrano'
+require 'bundler/capistrano'
+load 'deploy/assets'
+
 set :application, "ghint"
 set :repository,  "git://github.com/jarretraim/ghint.git"
 set :scm, :git
@@ -7,11 +13,21 @@ set :user, "ghint"
 set :scm_username, "jarretraim"
 ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "ghint_id_rsa")]
 
+default_run_options[:pty] = true
+
 set :deploy_to, "/opt/ghint"
 server "github-integration.inova.dfw1.us.ci.rackspace.net", :app, :web, :db, :primary => true
 
 # if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+after "deploy:restart", "deploy:cleanup"
+
+namespace :deploy do
+  task :symlink_shared do
+    run "ln -s #{shared_path}/local_env.yml #{release_path}/config/"
+  end
+end
+
+before "deploy:restart", "deploy:symlink_shared"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
