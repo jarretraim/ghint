fca88c45650531a93d88be6457a77d32fad3cfbe
Jarret Raim <jarret.raim@rackspace.com>
2013-01-01 11:39:12 -0600

    Logging Fixes

--- app/workers/remove_user.rb
+++ app/workers/remove_user.rb
@@ -6,19 +6,17 @@ class RemoveUser
   end
 
   def perform(sso)
-    puts "Attempting to remove #{sso} from GitHub access."
-
     user = User.find_by_username(sso)
 
     # Don't konw the user at all
     if user.nil?
-      puts "ERROR: SSO #{sso} is not a known Ghint user."
+      logger.error "SSO #{sso} is not a known Ghint user."
       return
     end
 
     # User logged in, but didn't associate a GH account
     if user.github_user.nil?
-      puts "ERROR: SSO #{sso} is not mapped to a user in Ghint - nothing to remove."
+      logger.error "SSO #{sso} is not mapped to a user in Ghint - nothing to remove."
       return
     end
 
@@ -27,12 +25,12 @@ class RemoveUser
     github = Github.new basic_auth: ENV['GH_USERNAME'] + ':' + ENV['GH_PASSWORD']
 
     if github.organizations.members.member? 'rackspace', gh_login
-      puts "User #{sso} is a member of the rackspace organization as #{gh_login}"
+      logger.debug "User #{sso} is a member of the rackspace organization as #{gh_login}"
 
       github.organizations.members.remove 'rackspace', gh_login
-      puts "Github user #{gh_login} has been removed from the rackspace organization."
+      logger.info "Github user #{gh_login} has been removed from the rackspace organization."
     else
-      puts "User #{sso} is NOT a member of the rackspace organization as #{gh_login}"
+      logger.info "User #{sso} is NOT a member of the rackspace organization as #{gh_login}"
     end
 
   end
