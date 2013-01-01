6682e91fb69278bc96b4c0e21ba3aff111d98ad2
Jarret Raim <jarret.raim@rackspace.com>
2013-01-01 11:34:48 -0600

    Remove required fields from github user

--- db/schema.rb
+++ db/schema.rb
@@ -16,14 +16,14 @@ ActiveRecord::Schema.define(:version => 20121223192627) do
   create_table "github_users", :force => true do |t|
     t.string   "login",         :null => false
     t.integer  "gh_id",         :null => false
-    t.string   "url",           :null => false
-    t.string   "html_url",      :null => false
-    t.string   "name",          :null => false
-    t.string   "location",      :null => false
-    t.string   "email",         :null => false
-    t.datetime "gh_created_at", :null => false
-    t.integer  "public_repos",  :null => false
-    t.integer  "private_repos", :null => false
+    t.string   "url"
+    t.string   "html_url"
+    t.string   "name"
+    t.string   "location"
+    t.string   "email"
+    t.datetime "gh_created_at"
+    t.integer  "public_repos"
+    t.integer  "private_repos"
     t.integer  "user_id"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
