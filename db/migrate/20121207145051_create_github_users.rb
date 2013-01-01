ef1e1160ddd6e71b54e3d0a168bf7019dfe8ea92
Jarret Raim <jarret.raim@rackspace.com>
2013-01-01 11:34:35 -0600

    Remove required fields from github user

--- db/migrate/20121207145051_create_github_users.rb
+++ db/migrate/20121207145051_create_github_users.rb
@@ -3,14 +3,14 @@ class CreateGithubUsers < ActiveRecord::Migration
     create_table :github_users do |t|
       t.string    :login,         :null => false
       t.integer   :gh_id,         :null => false
-      t.string    :url,           :null => false
-      t.string    :html_url,      :null => false
-      t.string    :name,          :null => false
-      t.string    :location,      :null => false
-      t.string    :email,         :null => false
-      t.datetime  :gh_created_at, :null => false
-      t.integer   :public_repos,  :null => false
-      t.integer   :private_repos, :null => false
+      t.string    :url
+      t.string    :html_url
+      t.string    :name
+      t.string    :location
+      t.string    :email
+      t.datetime  :gh_created_at
+      t.integer   :public_repos
+      t.integer   :private_repos
 
       t.references :user
 
